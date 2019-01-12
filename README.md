1fc026c117da3fe6acc03ca1e37735766ac8e34b
Cory Stein <corystein@gmail.com>
2019-01-12 15:01:00 -0500

    Added openldap

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# openldap-packer-terraform
+# Ansible Semaphore Packer/Terraform Deployment Scripts
+
+[![CircleCI](https://circleci.com/gh/corystein/semaphore-packer-terraform.svg?style=svg)](https://circleci.com/gh/corystein/semaphore-packer-terraform)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/2c2458413a574197b8cb1a787774761b)](https://www.codacy.com/app/corystein/semaphore-packer-terraform?utm_source=github.com&utm_medium=referral&utm_content=corystein/semaphore-packer-terraform&utm_campaign=Badge_Grade)
+
+## Links
+
+- [Releases](https://github.com/corystein/semaphore-packer-terraform/releases)
+- [Ansible Semaphore](https://github.com/ansible-semaphore/semaphore)
+- [Packer](https://packer.io)
+- [Terraform](https://terraform.io)
+- [Azure](https://portal.azure.com)
+
+## Introduction
+
+This project includes scripts to deploy [Ansible Semaphore](https://github.com/ansible-semaphore/semaphore) using [Packer](https://packer.io) and [Terraform](https://terraform.io). Packer script deploys the latest Ansible Semaphore into a single VM image.
+
+## Packer Image
+
+- Provider: [Azure](https://portal.azure.com)
+- OS: Linux CentOS 7.5
+- Ansible Semaphore: v2.5.1
+
+## Getting Started
+
+TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:
+
+1. Installation process
+2. Software dependencies
+3. Latest releases
+4. API references
+
+## Build and Test
+
+TODO: Describe and show how to build your code and run the tests.
+
+## Contributing
+
+PR's & UX reviews are welcome!
+
+Please follow the [contribution](https://github.com/ansible-semaphore/semaphore/blob/develop/CONTRIBUTING.md) guide. Any questions, please open an issue.
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2019 Cory Stein
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ packer/image-cleanup.ps1
@@ -0,0 +1,240 @@
+<#
+.DESCRIPTION
+    This script aids in retrieving and deleting images using the Azure CLI
+
+.INPUTS
+
+.OUTPUTS
+
+.EXAMPLE
+    # Remove images
+    ./image-cleanup.ps1 
+
+
+.LINK
+    https://docs.microsoft.com/en-us/cli/azure/image?view=azure-cli-latest#az-image-delete
+
+.NOTES
+
+    Prerequisites:
+        - PowerShell 5.1 or later
+        - Azure CLI
+
+    List all environment variables
+    Get-ChildItem Env:
+
+#>
+
+
+param(
+    [string] $SubscriptionId,
+    [string] $ResourceGroup = "PZI-GXUS-G-RGP-PADM-P001",
+    [int] $DaysToDelete = 7,
+    [string] $ImagePattern,
+    [switch] $KeepOnlyLatest
+)
+
+###################################################################################
+# IMPORTANT:  The below line must be included to enusre proper error handling
+###################################################################################
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+###################################################################################
+
+
+###################################################################################
+# Script Requirements
+###################################################################################
+
+if ([String]::IsNullOrWhiteSpace($(Get-Command az -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition))) {
+    throw "Azure CLI is missing" 
+}
+
+###################################################################################
+
+###################################################################################
+# BEGIN : FUNCTIONS
+###################################################################################
+function Get-PackerImages {
+    param(
+        $SubscriptionId,
+        $ResourceGroupName
+    )
+
+    Process {
+        # Get list of images from resource group
+        $result = az image list --resource-group $ResourceGroup --subscription "$($SubscriptionId)" | ConvertFrom-Json
+
+
+        # Enumerate through list
+        $objectCollection = @()
+        if ($result.Count -gt 0) {
+            $result | ForEach-Object {
+                #$_
+
+                # Create object
+                $object = New-Object PSObject
+                Add-Member -InputObject $object -MemberType NoteProperty -Name Name -Value ""
+                Add-Member -InputObject $object -MemberType NoteProperty -Name DateCreated -Value ""
+                
+                ######################################
+                # START : Construct Date
+                ######################################
+                $EndDate = $_.tags.create_time
+                $EndDateParts = $EndDate.Split("-")
+                $EndDateParts2 = $EndDateParts[2].Split("T")
+                $Hour = $EndDateParts2[1].Substring(0, 2)
+                $Minute = $EndDateParts2[1].Substring(2, 2)
+                $HourMinute = "{0}:{1}" -f $Hour, $Minute
+                $EndDateTime = "{0:hh:mm tt}" -f [datetime]$HourMinute
+                $EndDatePart = [datetime]$("{0}/{1}/{2} {3}" -f $EndDateParts[1], $EndDateParts2[0], $EndDateParts[0], $EndDateTime)
+                ######################################
+                # END : Construct Date
+                ######################################
+
+                # Add value to properties
+                $object.Name = $_.name
+                $object.DateCreated = $EndDatePart
+
+                # Add object to collection
+                $objectCollection += $object
+            }
+        }
+
+        return $objectCollection
+
+    }
+}
+
+function Get-LatestPackerImage {
+    param(
+        $imageList,
+        $pattern
+    )
+
+    Process {
+        return $($imageList | Sort-Object DateCreated | Select-Object -Last 1 | Select-Object -Property Name).Name
+    }
+}
+
+function main() {
+    try {
+
+        # Get subscription id from current token/session
+        if ([String]::IsNullOrWhiteSpace($SubscriptionId)) {
+            $azureRmContext = $(az account show) | ConvertFrom-Json
+            $SubscriptionId = $azureRmContext.id
+        }
+
+        ###################################################################
+        # Display Parameter Values
+        ###################################################################
+        Write-Output "Subscription Id: ......................... [$($SubscriptionId)]"
+        Write-Output "Resource Group Name: ..................... [$($ResourceGroup)]"
+        Write-Output "Days To Delete: .......................... [$($DaysToDelete)]"
+        Write-Output "Keep Only Latest: ........................ [$($KeepOnlyLatest)]" 
+        ###################################################################
+
+        <#
+        # Get list of images from resource group
+        $result = az image list --resource-group $ResourceGroup --subscription "$($SubscriptionId)" | ConvertFrom-Json
+
+        # Enumerate through list
+        $objectCollection = @()
+        if ($result.Count -gt 0) {
+            $result | ForEach-Object {
+                #$_
+
+                # Create object
+                $object = New-Object PSObject
+                Add-Member -InputObject $object -MemberType NoteProperty -Name Name -Value ""
+                Add-Member -InputObject $object -MemberType NoteProperty -Name DateCreated -Value ""
+                
+                ######################################
+                # START : Construct Date
+                ######################################
+                $EndDate = $_.tags.create_time
+                $EndDateParts = $EndDate.Split("-")
+                $EndDateParts2 = $EndDateParts[2].Split("T")
+                $Hour = $EndDateParts2[1].Substring(0, 2)
+                $Minute = $EndDateParts2[1].Substring(2, 2)
+                $HourMinute = "{0}:{1}" -f $Hour, $Minute
+                $EndDateTime = "{0:hh:mm tt}" -f [datetime]$HourMinute
+                $EndDatePart = [datetime]$("{0}/{1}/{2} {3}" -f $EndDateParts[1], $EndDateParts2[0], $EndDateParts[0], $EndDateTime)
+                ######################################
+                # END : Construct Date
+                ######################################
+
+                # Add value to properties
+                $object.Name = $_.name
+                $object.DateCreated = $EndDatePart
+
+                # Add object to collection
+                $objectCollection += $object
+            }
+        }
+        #>
+
+        $packerImages = Get-PackerImages -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroup
+
+        # Get most recent image
+        #$LatestImage = $($objectCollection | Sort-Object DateCreated | Select-Object -Last 1 | Select-Object -Property Name).Name
+        $LatestImage = Get-LatestPackerImage -imageList $packerImages
+
+        ############################################################
+        # START : Process images for deletion
+        ############################################################
+        [int]$Deleted = 0
+        $StartDate = (GET-DATE)
+
+        if ($KeepOnlyLatest) {
+            Write-Verbose "Deleting all images except latest"
+            $packerImages | Where-Object { $_.Name -ne $LatestImage } | ForEach-Object {
+                Write-Output "Deleting image [$($_.name)]..."
+                az image delete --name "$($_.name)" --resource-group $ResourceGroup --subscription "$($SubscriptionId)"
+                $Deleted++
+                Write-Output "Successfully deleted image"
+            }
+        }
+        else {
+            Write-Verbose "Deleting images older than days specified"
+            $packerImages | ForEach-Object {
+                $ts = New-TimeSpan –Start $StartDate –End $EndDatePart
+                if ($ts.Days -le ($DaysToDelete * -1)) {
+                    Write-Warning "Image older than $DaysToDelete day(s)"
+                    Write-Output "Deleting image [$($_.name)]..."
+                    az image delete --name "$($_.name)" --resource-group $ResourceGroup --subscription "$($SubscriptionId)"
+                    $Deleted++
+                    Write-Output "Successfully deleted image"
+                }
+            }
+        }
+
+        # Display number of images deleted
+        if ($Deleted -gt 0) {
+            Write-Warning "Deleted [$Deleted] images"
+            Write-Output "Completed deleting images"
+        }
+        else {
+            Write-Output "No images to delete"
+        }
+        ############################################################
+        # END : Process images for deletion
+        ############################################################
+
+    }
+    catch {
+        Write-Error $_.Exception.Message
+        throw
+    }
+    finally {
+        Pop-Location
+    }
+} 
+
+###################################################################################
+# END : FUNCTIONS
+###################################################################################
+
+# Call main
+main
